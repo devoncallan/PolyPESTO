620fc4b1e9d7b37773bd355f24ccf5162d9a2d05
Devon <devoncallan@ucsb.edu>
2025-04-03 18:54:42 +0000

    Add ensemble / predictions

--- polypesto/core/pypesto.py
+++ polypesto/core/pypesto.py
@@ -1,37 +1,24 @@
 from typing import Dict, Tuple, List, Optional, Any, Union, TypeAlias
 import os
 
+import numpy as np
 from amici.petab.simulations import simulate_petab, rdatas_to_measurement_df
 from amici.amici import Solver
+import pypesto.optimize
 from pypesto.petab import PetabImporter
 from pypesto.problem import Problem
 from pypesto import Result
 import pypesto
 import pypesto.optimize as optimize
-import pypesto.petab
-import pypesto.sample as sample
-import pypesto.profile as profile
-import pypesto.visualize as visualize
 
-from polypesto.core.petab import PetabData, PetabIO
-from polypesto.core.params import (
-    ParameterGroup,
-    ParameterSet,
-    ParameterID,
-    ParameterSetID,
-)
-from polypesto.models import sbml, ModelInterface
+from pypesto.ensemble import Ensemble, EnsemblePrediction
+from pypesto.C import EnsembleType
+from pypesto.objective import AmiciObjective
+from pypesto.C import AMICI_STATUS, AMICI_T, AMICI_X, AMICI_Y
+from pypesto.predict import AmiciPredictor
+import pypesto.sample
+
 from polypesto.core.experiment import Experiment
-
-# from experiments.irreversible_cpe.single_rxn.experiments import ExperimentData
-
-# Type aliases for better code clarity
-# ExperimentName: TypeAlias = str
-# ExperimentCollection: TypeAlias = Dict[ExperimentName, Experiment]
-# ResultDict: TypeAlias = Dict[ParameterSetID, Result]
-# ExperimentResults: TypeAlias = Dict[ExperimentName, ResultDict]
-# StepConfigDict: TypeAlias = Dict[str, Any]
-# PEConfigDict: TypeAlias = Dict[str, StepConfigDict]
 
 
 def set_amici_solver_params(problem: Problem) -> Problem:
@@ -108,7 +95,7 @@ def optimize_problem(problem: Problem, method: str = "Nelder-Mead", **kwargs) ->
     Result
         Optimization result object containing best parameters and history
     """
-    optimizer = optimize.ScipyOptimizer(method=method)
+    optimizer = pypesto.optimize.ScipyOptimizer(method=method)
     history_options = pypesto.HistoryOptions(trace_record=True)
     engine = pypesto.engine.MultiProcessEngine()
 
@@ -143,8 +130,10 @@ def profile_problem(
     Result
         Updated result object containing parameter profiles
     """
-    optimizer = optimize.ScipyOptimizer(method=method)
-    result = profile.parameter_profile(problem=problem, optimizer=optimizer, **kwargs)
+    optimizer = pypesto.optimize.ScipyOptimizer(method=method)
+    result = pypesto.profile.parameter_profile(
+        problem=problem, optimizer=optimizer, **kwargs
+    )
     return result
 
 
@@ -172,12 +161,12 @@ def sample_problem(
     Result
         Updated result object containing parameter samples
     """
-    sampler = sample.AdaptiveParallelTemperingSampler(
-        internal_sampler=sample.AdaptiveMetropolisSampler(),
+    sampler = pypesto.sample.AdaptiveParallelTemperingSampler(
+        internal_sampler=pypesto.sample.AdaptiveMetropolisSampler(),
         n_chains=n_chains,
     )
 
-    result = sample.sample(
+    result = pypesto.sample.sample(
         problem=problem, n_samples=n_samples, sampler=sampler, **kwargs
     )
 
@@ -191,5 +180,78 @@ def save_result(result: Result, filename: str, **kwargs):
     )
 
 
-def create_ensemble():
-    pass
+def create_ensemble(exp: Experiment, result: Result) -> Ensemble:
+
+    problem = exp.pypesto_problem
+    x_names = problem.get_reduced_vector(problem.x_names)
+
+    ensemble = Ensemble.from_sample(
+        result=result,
+        remove_burn_in=False,
+        chain_slice=slice(None, None, 5),
+        x_names=x_names,
+        ensemble_type=EnsembleType.sample,
+        lower_bound=result.problem.lb,
+        upper_bound=result.problem.ub,
+    )
+
+    return ensemble
+
+
+def create_predictor(exp: Experiment, output_type: str) -> AmiciPredictor:
+
+    obj: AmiciObjective = exp.pypesto_problem.objective
+
+    if output_type == AMICI_Y:
+        output_ids = obj.amici_model.getObservableIds()
+    elif output_type == AMICI_X:
+        output_ids = obj.amici_model.getStateIds()
+    else:
+        raise ValueError(f"Unknown output type: {output_type}")
+
+    # This post_processor will transform the output of the simulation tool
+    # such that the output is compatible with the next steps.
+    def post_processor(amici_outputs, output_type, output_ids):
+        outputs = [
+            (
+                amici_output[output_type]
+                if amici_output[AMICI_STATUS] == 0
+                else np.full((len(amici_output[AMICI_T]), len(output_ids)), np.nan)
+            )
+            for amici_output in amici_outputs
+        ]
+        return outputs
+
+    from functools import partial
+
+    post_processor_bound = partial(
+        post_processor,
+        output_type=output_type,
+        output_ids=output_ids,
+    )
+
+    predictor = AmiciPredictor(
+        amici_objective=obj,
+        post_processor=post_processor_bound,
+        output_ids=output_ids,
+    )
+
+    return predictor
+
+
+def predict_with_ensemble(
+    ensemble: Ensemble,
+    test_exp: Experiment,
+    output_type: str = AMICI_Y,
+    **kwargs,
+) -> EnsemblePrediction:
+
+    predictor = create_predictor(test_exp, output_type)
+
+    engine = pypesto.engine.MultiProcessEngine(**kwargs)
+    ensemble_pred = ensemble.predict(
+        predictor=predictor,
+        prediction_id=output_type,
+        engine=engine,
+    )
+    return ensemble_pred
