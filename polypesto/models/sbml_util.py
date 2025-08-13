8ba782dfa50d9d157472d4b0ccb9d22bd1f17caa
Shivani Madhan <madhan.shivani1104@gmail.com>
2025-08-12 17:30:57 -0700

    removing docker file and making code pip installable

--- polypesto/models/sbml_util.py
+++ polypesto/models/sbml_util.py
@@ -2,7 +2,6 @@ from typing import Dict, Tuple, Callable, List, TypeAlias, Optional
 import os
 import time
 import libsbml
-import SBMLDiagrams
 
 SBML_LEVEL = 3
 SBML_VERSION = 2
@@ -26,14 +25,6 @@ def write_model(model_def: ModelDefinition, model_dir: str) -> str:
     return model_filepath
 
 
-def visualize_model(model_filename: str, output_filename: str = "Model.jpg") -> None:
-
-    df = SBMLDiagrams.load(model_filename)
-
-    # df.autolayout()
-    df.draw(output_fileName=output_filename)
-
-
 def _save_sbml(document: Document, model_filepath: str):
     """Outputs the given model string to the given filename."""
     model_xml_string = libsbml.writeSBMLToString(document)
@@ -44,14 +35,6 @@ def _save_sbml(document: Document, model_filepath: str):
 
     with open(model_filepath, "w") as f:
         f.write(model_xml_string)
-
-
-def visualize_model(model_filename: str, output_filename: str = "Model.jpg") -> None:
-
-    df = SBMLDiagrams.load(model_filename)
-
-    # df.autolayout()
-    df.draw(output_fileName=output_filename)
 
 
 #####################################
