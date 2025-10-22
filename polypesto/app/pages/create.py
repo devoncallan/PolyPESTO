a38cc8721f192ea4fc4a995e74bf11011ca929e6
Devon <devoncallan@ucsb.edu>
2025-10-22 14:49:35 -0700

    multipage app

---
+++ polypesto/app/pages/create.py
@@ -0,0 +1,6 @@
+
+
+import streamlit as st
+
+
+st.write("Create New Study Page - Under Construction")
--- polypesto/app/pages/load.py
+++ polypesto/app/pages/load.py
@@ -64,7 +64,7 @@ fig_type = c2.selectbox(
 )
 fig_paths = study_comp.get_figure_paths(study_key, fig_type=fig_type)
 
-cols = st.columns(len(fig_paths)+2)
+cols = st.columns(len(fig_paths) + 2)
 for (study_name, fig_path), col in zip(fig_paths.items(), cols):
     col.markdown(f"### {study_name}")
     image = fig_path.read_bytes()
